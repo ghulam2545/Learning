7a1817b093ea6f8649bac4bf0c61af5d491987d1
ghulam2545 <ghulam2545@gmail.com>
2022-02-25 12:31:36 +0530

    Hello in asm

---
+++ Cpp/assembly/Hello.s
@@ -0,0 +1,24 @@
+.Hello:
+	.ascii "Hello World\12\0"
+	.text
+	.globl	main
+	.def	main
+	.seh_proc	main
+main:
+.start:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	call	__main
+	leaq	.Hello(%rip), %rax
+	movq	%rax, %rcx
+	call	printf
+	movl	$0, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
